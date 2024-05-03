3a29557ec913707702d61ce607fd6c3b0db2ab7e
valentttino <vgboggio@gmail.com>
2024-05-03 00:20:30 -0300

    ex 3.9 done and CORS was added

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
 app.use(express.json())
 app.use(morgan('tiny'))
-
+app.use(cors())
 
 let persons = [
     { 
