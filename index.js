8aa2b706b5c1b2adcb5b396e25d212f0785f203d
valentttino <vgboggio@gmail.com>
2024-05-04 23:24:41 -0300

    ex 3.13 done. MongoDB in the app

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const express = require('express')
+require('dotenv').config() // dotenv for enviroment variables
+const express = require('express') // import express
 const app = express()
-const morgan = require('morgan')
-const cors = require('cors')
+const morgan = require('morgan') // import morgan for msgs in console
+const cors = require('cors') // import cors
+const Person = require('./models/person') // import person module
+
 
 app.use(express.json())
 app.use(morgan('tiny'))
@@ -39,7 +42,9 @@ let cont = 0
 
 app.get('/api/persons', (request, response) =>{
     cont++
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 app.get('/info', (request, response) =>{ 
@@ -48,13 +53,9 @@ app.get('/info', (request, response) =>{
 })
 
 app.get('/api/persons/:id', (request, response) =>{
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person){
+    Person.findById(request.params.id).then(person => {
         response.json(person)
-    }else{
-        response.status(404).end()
-    }
+    })
 })
 
 app.delete('/api/persons/:id', (request, response) =>{
@@ -107,7 +108,7 @@ app.post('/api/persons', (request, response) =>{
     response.json(person)
 })
 
-const PORT = process.env.PORT || 3002
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
