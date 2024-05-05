1869f6c8343a35d07adc8eb7ede03391681b7735
valentttino <vgboggio@gmail.com>
2024-05-05 00:34:29 -0300

    ex 3.15 done.

--- index.js
+++ index.js
@@ -11,28 +11,15 @@ app.use(morgan('tiny'))
 app.use(cors())
 app.use(express.static('dist'))
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+const requestLogger = (request, response, next) =>{
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
+
+app.use(requestLogger)
 
 app.get('/', (request, response) =>{
     response.send('<h1> Hello world.. </h1> <h2> This API was made by Valentino Boggio </h2> <p> In progress.. </p>')
@@ -64,11 +51,12 @@ app.get('/api/persons/:id', (request, response, next) =>{
         .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) =>{
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) =>{
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 // const findName = (name) =>{
