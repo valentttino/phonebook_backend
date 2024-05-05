4ad695d652717a487b7f9d2dc7b474823f2fbdb1
valentttino <vgboggio@gmail.com>
2024-05-04 23:54:44 -0300

    ex 3.16 done

--- index.js
+++ index.js
@@ -52,10 +52,16 @@ app.get('/info', (request, response) =>{
     response.send(`Phonebook has info for ${cont} people <br/> <br/> ${hora}`)
 })
 
-app.get('/api/persons/:id', (request, response) =>{
-    Person.findById(request.params.id).then(person => {
-        response.json(person)
-    })
+app.get('/api/persons/:id', (request, response, next) =>{
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person){
+                response.json(person)
+            } else{
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response) =>{
@@ -79,9 +85,7 @@ app.post('/api/persons', (request, response) =>{
     const body = request.body
 
     if(!body.name || !body.number){
-        return response.status(400).json({
-            error: 'content missing'
-        })
+        return response.status(400).json({error: 'content missing'})
     }
 
     // if(findName(body.name)){
@@ -99,6 +103,24 @@ app.post('/api/persons', (request, response) =>{
         response.json(savedPerson)
     })
 })
+
+const unknownEndpoint = (request, response) =>{
+    response.status(404).send({error:'unknown endpoint'})
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) =>{
+    console.error(error.mnessage)
+
+    if (error.name === 'CastError'){
+        return response.status(400).send({error: 'malformatted id'})
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
